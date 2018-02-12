b35450b2f95cdfad6d3d42173da28673383bd5b5
Tanel Kriik <tanel.kriik+github@gmail.com>
2018-02-12 21:48:39 +0200

    Initiate app."

--- index.js
+++ index.js
@@ -107,4 +107,7 @@ function error(message) {
   }
 }
 
-app.listen(3001, () => console.log('Listening on port 3001'))
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log('Listening on port 3001')
+})
